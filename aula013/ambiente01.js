208f194a07be3833ab4823b632b3e486ce401dc1
thomasmagela01 <thomasmagela01@gmail.com>
2023-07-06 18:27:13 -0300

    aula013

---
+++ aula013/ambiente01.js
@@ -0,0 +1,7 @@
+var c = 1 
+
+// Enquanto
+while (c <= 5) { 
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
---
+++ aula013/ambiente02.js
@@ -0,0 +1,7 @@
+var c = 1 
+
+// Faça
+do  { 
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
