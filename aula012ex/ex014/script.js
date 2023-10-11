9576ac5548d247a6b81f7fc6616a1c44f1d608c0
Thomas Magela <134350374+thomasmagela01@users.noreply.github.com>
2023-10-11 12:20:33 -0300

    Update script.js

--- aula012ex/ex014/script.js
+++ aula012ex/ex014/script.js
@@ -3,7 +3,6 @@ function carregar() {
     var img = window.document.getElementById('img')
     var data = new Date()
     var hora = data.getHours()
-    //var hora = 9
 
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
